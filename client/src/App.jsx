f82b2917275ba1aa5f0c9c7f11226ebf05820dca
Nidhithakor <thakornidhi5@gmail.com>
2025-09-23 10:00:43 +0530

    added clerk auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Rpotes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Feed from "./pages/Feed";
 import Messages from "./pages/Messages";
@@ -8,15 +8,19 @@ import Connections from "./pages/Connections";
 import Discover from "./pages/Discover";
 import Profile from "./pages/Profile";
 import CreatePost from "./pages/CreatePost";
-
+import {useUser} from '@clerk/clerk-react'
+import Layout from './pages/Layout'
 
 
 const App = () => {
+
+    const {user} = useUser()
+
   return (
     <>
       <Routes>
-        <Route path='/' element={<Login />}></Route>
-        <Route index element={<Feed />} />
+        <Route path='/'  element={ !user ? <Login /> : <Layout />} />
+        <Route path='feed' element={<Feed />} />
         <Route path='messages' element={<Messages />} />
         <Route path='messages/:userId' element={<ChatBox />} />
         <Route path='connections' element={<Connections />} />
