84fba1ab528b524fd9b375cfec2938cfd125f26b
Nidhithakor <thakornidhi5@gmail.com>
2025-09-25 11:15:44 +0530

    added postcard component and render the postcard componenet to feed page to display posts

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { dummyPostsData } from '../assets/assets'
 import Loading from '../components/Loading'
 import StoriesBar from '../components/StoriesBar'
+import PostCard from '../components/PostCard'
 
 const Feed = () => {
 
@@ -22,7 +23,11 @@ const Feed = () => {
       {/* stories and post list */}
       <div>
         <StoriesBar />
-        <div className="p-4 space-y-6">List of post</div>
+        <div className="p-4 space-y-6">
+          {feeds.map((post) => (
+            <PostCard key={post._id}  post={post}/>
+          ))}
+        </div>
       </div>
 
       {/* right sidebar  */}
